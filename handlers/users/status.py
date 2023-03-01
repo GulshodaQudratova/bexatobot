cb426e42d1b0eff932f3981d10a7520f66492bf9
Gulshoda <samiraqudratova809@gmail.com>
2023-03-01 23:24:53 +0500

    bexatobot

--- handlers/users/status.py
+++ handlers/users/status.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp,Command
 
-from api import get_info
+from api import get_users
 from loader import dp
 @dp.message_handler(Command('info'))
 async def info(message:types.Message):
